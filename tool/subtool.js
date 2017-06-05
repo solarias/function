82761651d400d6b022319f41697aaf28f48bdfd2
solarias <ansewo@naver.com>
2017-06-06 00:23:05 +0900

    각종 업데이트

--- tool/subtool.js
+++ tool/subtool.js
@@ -44,7 +44,50 @@ if("document" in self){if(!("classList" in document.createElement("_"))){(functi
 //isArray 호환용 (대상 : IE8)
 if (!Array.isArray){Array.isArray=function(arg){return Object.prototype.toString.call(arg)==='[object Array]';};}
 
-
+//repeat 호환용 (대상 : IE 전체)
+if (!String.prototype.repeat) {
+  String.prototype.repeat = function(count) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    // Could we try:
+    // return Array(count + 1).join(this);
+    return rpt;
+  }
+}
 
 //=================================================================================================================
 //※ 함수 - requst시리즈
@@ -250,6 +293,27 @@ function setWon(pWon) {
     var arrCnt = won.split(",").length-1;
     for(var ii=0; ii<won.split(",").length; ii++) {
         changeWon += won.split(",")[ii]+arrWon[arrCnt];
+        arrCnt--;
+    }
+    return changeWon;
+}
+
+//만단위 한글로 전환 (0000은 표시하지 않기)
+function setWon2(pWon) {
+    //null, undefined : "0" 리턴
+    if (!pWon) return "0";
+
+    var won  = pWon.toString();
+    var arrWon  = ["", "만 ", "억 ", "조 ", "경 ", "해 ", "자 ", "양 ", "구 ", "간 ", "정 "];
+    var changeWon = "";
+    var pattern = /(-?[0-9]+)([0-9]{4})/;
+    while(pattern.test(won)) {
+        won = won.replace(pattern,"$1,$2");
+    }
+    won = won + "";
+    var arrCnt = won.split(",").length-1;
+    for(var ii=0; ii<won.split(",").length; ii++) {
+        if (won.split(",")[ii] != "0000") changeWon += won.split(",")[ii]+arrWon[arrCnt];
         arrCnt--;
     }
     return changeWon;
